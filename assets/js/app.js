09b96672e74b377270deb059e17a77f164dac9e7
Quan Huynh <qlhuynh@gmail.com>
2020-01-31 13:22:05 -0800

    add category fetch

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,23 @@ const getCategories = () => {
   fetch(`https://opentdb.com/api_category.php`)
     .then(r => r.json())
     .then((response) => {
-      //let myDiv = getElementById('categories')
+      
       console.log(response)
-      // trivia_categories.forEach(e)=>
-      // {
+      let category = document.getElementById('category-dropdown')
+      let { trivia_categories: categoryArray } = response
+      categoryArray.forEach((e) => {
+        let button = document.createElement('a')
+        button.className = 'dropdown-item'
+        button.text = e.name
+        button.id = e.id
+        button.href='#'
+        button.value = e.id
+        category.append(button)
+        console.log(button)
+      })
 
-      //   console.log(e.id, e.name)
-      // }       
+      console.log(category)
+
     })
     .catch(e => console.error(e))
 }
